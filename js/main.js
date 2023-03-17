ffa6ddde9a1a65ec4a9f43502e8a3e2add0e66a0
BijanJab <bijanjab@gmail.com>
2023-03-17 12:54:47 +0330

    add privacy

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["We develop and publish games", "Hyper-Casual and Hybrid-Casual", "Love Simplifying complexities "],
         typeSpeed: 100,
         backDelay: 900,
         // loop
